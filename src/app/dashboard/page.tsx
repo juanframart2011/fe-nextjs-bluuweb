12ce8640f1c8d0c327e36442b047940c5d955390
juanframart2011 <juanframart2011@gmail.com>
2025-01-10 22:37:07 -0600

    ajustando jwt

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,39 @@
-const Page = () => {
+"use client";
+import { useSession } from "next-auth/react";
+
+const Dashboard = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  console.log(session?.user?.token);
+
+  const getCats = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/cats`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.user?.token}`,
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1 className="text-primary">Dashboard Page</h1>
+      <h1>Dashboard</h1>
+      <button
+        onClick={getCats}
+        className="btn btn-primary"
+      >
+        Get Cats
+      </button>
+      <pre>
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
     </div>
   );
-}
-export default Page;
+};
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import ButtonAuth from "@/components/ButtonAuth";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-primary">Home Page</h1>
+      <h1>Home Page</h1>
       <ButtonAuth />
     </div>
   );
-}
+};
 export default HomePage;
