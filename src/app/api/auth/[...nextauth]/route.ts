6843a89d27358f2564ed97105582d02d925b00de
juanframart2011 <juanframart2011@gmail.com>
2025-01-11 19:45:30 -0600

    ajuste de login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,26 @@ import CredentialsProvider from "next-auth/providers/credentials";
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
-      ...
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
+          method: "POST",
+          body: JSON.stringify({ email: credentials?.email, password: credentials?.password }),
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        const user = await res.json();
+        if(user.error) {
+          throw new Error(user.error);
+        }
+        return user;
+      },
+    })
   ],
   callbacks: {
     async jwt({ token, user }) {
